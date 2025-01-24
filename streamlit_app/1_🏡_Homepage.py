5d4cf596131eea66ac058d1a939035cd09d7b878
shrutidhage91 <dhageshruti91@gmail.com>
2025-01-24 12:01:48 +0530

    Add files via upload

---
+++ streamlit_app/1_🏡_Homepage.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import google.generativeai as genai
+st.title("Welcome to Data Nexus!")
+st.write("Accelerate data engineering with data us!!")
