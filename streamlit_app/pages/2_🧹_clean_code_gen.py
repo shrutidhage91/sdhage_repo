9322791bff349cd950f0913fadf49cfeb4970819
shrutidhage91 <dhageshruti91@gmail.com>
2025-01-24 12:03:27 +0530

    Add files via upload

---
+++ streamlit_app/pages/2_🧹_clean_code_gen.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import google.generativeai as genai
+st.title("Clean code generator")
+st.write("<build you clean code generator app here>")
---
+++ streamlit_app/pages/3_💻_Data_normalizer.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import google.generativeai as genai
+st.title("data normalization")
+st.write("<build you data normalization app here>")
